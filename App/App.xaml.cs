1b4992432c807e2a70b1516ffc1742a0572787b8
William Cowden <williamcowden39@gmail.com>
2024-04-27 17:46:35 -0500

    Initial App Upload
    This is the initial upload of the .NET MAUI android app.

---
+++ App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EnterpriseMarketplace
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EnterpriseMarketplace
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
