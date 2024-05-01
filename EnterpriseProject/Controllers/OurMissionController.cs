3083d73317a0440e07c35da39dc45ab9e15a3593
jdog01 <jbm19@students.ptcollege.edu>
2024-05-01 17:28:38 -0400

    Create the Our Mission view

---
+++ EnterpriseProject/Controllers/OurMissionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriseProject.Controllers
+{
+    public class OurMissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
