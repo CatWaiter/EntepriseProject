c3eeb1d74e8101e8a3f1047a7a7471c28e95a5ab
William Cowden <williamcowden39@gmail.com>
2024-05-27 15:42:04 -0400

    API creation
    Created API in c# using asp.net has CRUD functions for users, listings, and saved listings.

---
+++ EnterpriseAPI/Models/SavedListing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EnterpriseAPI.Models
+{
+    public class SavedListing
+    {
+        public int SavedListingId { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public int ListingId { get; set; }
+        public Listing? Listing { get; set; }
+        public DateTime SavedDate { get; set; }
+    }
+}
